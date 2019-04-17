e1d1ea2a311f7b427f7453c336014f4affb3d479
redapple67 <fahim.private@gmail.com>
2019-04-17 14:42:45 -0500

    Reading Input Data

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -5,16 +5,39 @@ var budgetController = (function(){
 
 var UIController = (function(){
 
+    var DomStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getinput: function(){
+            return{
+             type: document.querySelector(DomStrings.inputType).value, // inc pr exp
+             descritption: document.querySelector(DomStrings.inputDescription).value,
+             value: document.querySelector(DomStrings.inputValue).value
+            };        
+        },
+
+        getDomStrings: function(){
+            return DomStrings;
+        }
+    };
 })();
 
 
 // Global App Controller
 var controller = (function(budgetCtrl, UICtrl){
 
-    var ctrlAddItem = function() {
-        console.log('it works');
-        //1. Get the field input data
+    var Dom = UICtrl.getDomStrings();
 
+    var ctrlAddItem = function() {
+        
+        //1. Get the field input data
+        var input = UICtrl.getinput();
+        console.log(input);
         //2. Add item to budget controller
 
         //3. Add the item to ui
@@ -22,9 +45,10 @@ var controller = (function(budgetCtrl, UICtrl){
         //4. Calulate the budget
 
         //5. Display the budget on the UI
+       
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(Dom.inputBtn).addEventListener('click', ctrlAddItem);
 
 
     document.addEventListener('keypress', function(event){
